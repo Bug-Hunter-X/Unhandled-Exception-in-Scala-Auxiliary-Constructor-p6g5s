533f4efe679e9a7f94b7af832479d58fe3cc5983
BugHunter-X <askzenai@gmail.com>
2025-01-06 07:52:06 +0100

    Add bug.scala file

---
+++ bug.scala
@@ -0,0 +1,22 @@
+```scala
+class MyClass(val x: Int) {
+  def this() = this(0) // Auxiliary constructor
+
+  def myMethod(y: Int): Int = {
+    if (y == 0) {
+      throw new IllegalArgumentException("y cannot be 0")
+    } else {
+      x + y
+    }
+  }
+}
+
+object Main extends App {
+  val obj1 = new MyClass(5)
+  val obj2 = new MyClass()
+
+  println(obj1.myMethod(2)) // Output: 7
+  println(obj2.myMethod(2)) // Output: 2
+  println(obj1.myMethod(0)) // Throws IllegalArgumentException
+}
+```
